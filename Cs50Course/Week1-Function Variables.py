0c6c825dec5bccd936837afab1dc4dd7049caef0
= <lubenjazlyn35@gmail.com>
2024-04-23 23:26:38 -0400

    Starting CS50 course, for python.

---
+++ Cs50Course/Week1-Function Variables.py
@@ -0,0 +1,10 @@
+# User Input  - getting  value from user
+name = input("What's your name?")
+
+
+#Output user input - output the value stored.
+print("Hey," + name)
+
+
+#print hello world
+print("Hello World")
